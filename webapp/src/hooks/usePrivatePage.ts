909f5a573eb70e37bc499795c90c12d9abb0ff58
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2022-09-18 20:25:09 -0300

    Fix deps of usePrivatePage

--- webapp/src/hooks/usePrivatePage.ts
+++ webapp/src/hooks/usePrivatePage.ts
@@ -5,12 +5,11 @@ import {useEffect} from "react";
 
 export function usePrivatePage(){
 	const {isAuthenticated} = useAuth()
-
 	const navigate = useNavigate()
 
 	useEffect(() => {
 		if(!isAuthenticated){
 			navigate({to: "/login"})
 		}
-	},[])
+	},[isAuthenticated])
 }
